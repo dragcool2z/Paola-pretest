bfb57e667fa03b2ed5ce306b1c39361f1430c588
Galih A Ramitan <balmough@gmail.com>
2019-09-03 16:58:09 +0700

    Add files via upload

---
+++ build/precache-manifest.87bcec8f3444132f9d51774f6751419b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b99208b861652d4aca39ad0fd8ed2b4e",
+    "url": "//github.com/dragcool2z/Paola-fe-pretest/index.html"
+  },
+  {
+    "revision": "63a51d55178e8959649b",
+    "url": "//github.com/dragcool2z/Paola-fe-pretest/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "1b7fba12d2a96191d5ae",
+    "url": "//github.com/dragcool2z/Paola-fe-pretest/static/js/2.27c935f6.chunk.js"
+  },
+  {
+    "revision": "63a51d55178e8959649b",
+    "url": "//github.com/dragcool2z/Paola-fe-pretest/static/js/main.e200539f.chunk.js"
+  },
+  {
+    "revision": "3f0ce6ab018347d7ff21",
+    "url": "//github.com/dragcool2z/Paola-fe-pretest/static/js/runtime~main.9b33a2a8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "//github.com/dragcool2z/Paola-fe-pretest/static/media/logo.5d5d9eef.svg"
+  }
+]);
